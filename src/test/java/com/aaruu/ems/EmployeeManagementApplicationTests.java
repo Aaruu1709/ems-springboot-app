3bfcd41be107068668dfa255b6f0c7df4a5efc45
Shinde Arti <artishinde1700@gmail.com>
2026-05-31 00:41:28 +0530

    Setup Spring Boot project with MySQL connection

---
+++ src/test/java/com/aaruu/ems/EmployeeManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aaruu.ems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
