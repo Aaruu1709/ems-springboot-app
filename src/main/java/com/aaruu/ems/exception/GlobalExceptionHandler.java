27e35bd6728d0d2e16ffcae77e54fa93577ea076
Shinde Arti <artishinde1700@gmail.com>
2026-06-02 09:43:20 +0530

    add custom exception handling code

--- src/main/java/com/aaruu/ems/exception/GlobalExceptionHandler.java
+++ src/main/java/com/aaruu/ems/exception/GlobalExceptionHandler.java
@@ -1,8 +1,30 @@
 package com.aaruu.ems.exception;
 
-import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 
+	@ExceptionHandler(EmployeeNotFoundException.class)
+	@ResponseStatus(HttpStatus.NOT_FOUND)
+
+	public String handleEmployeeNotFoundException(EmployeeNotFoundException ex) {
+
+		return ex.getMessage();
+	}
 }
+
+//Employee not found
+//↓
+//orElseThrow()
+//↓
+//throws EmployeeNotFoundException
+//↓
+//Spring automatically looks for a handler
+//↓
+//GlobalExceptionHandler catches it
+//↓
+//returns proper response (404 + message)
